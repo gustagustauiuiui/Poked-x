0da66daee2b40be184fd30d51b6407b0576d33dc
Gustavo Lage <lageg901@gmail.com>
2023-12-16 20:05:01 -0400

    showPokemons

--- script.js
+++ script.js
@@ -1,52 +1,90 @@
 const pokemons = [];
+const pokemonsLista = [];
 
-async function getPokemon(name) {
-    const results = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+async function getPokemon(nameOrID) {
+    const results = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameOrID}/`);
 
-    return results.json();
+    return await results.json();
 }
 
-async function getPokemons(limit){
+async function getPokemons(limit) {
     const result = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0.`);
 
-    return result.json();
+    return await result.json();
 }
 
-function storePokemon(name){
+async function storePokemon(nameOrID) {
 
-    getPokemon(name).then(pokemon => {
-        pokemons.push({
+    await getPokemon(nameOrID).then(pokemon => {
+        let pokemonTratado = {
             id: pokemon.id,
             name: pokemon.name,
             height: pokemon.height,
             weight: pokemon.weight,
             sprite: pokemon.sprites.other['official-artwork'].front_default,
             abilities: pokemon.abilities.map(a => {
-                return {ability: {name:  a.ability.name}}
+                return { ability: { name: a.ability.name } }
             }),
             types: pokemon.types.map((t) => {
-                return {type: t.type.name};
-            }),
-        });
-        
+                return { type: t.type.name };
+            })
+        };
+
+        pokemons.push(pokemonTratado);
+        exibir(pokemonTratado);
+
     }).catch((error) => console.log(error));
 }
 
-function consultPokemons(qtd){
+async function consultPokemons(qtd) {
 
-    getPokemons(qtd).then(pokemons => {
+    await getPokemons(qtd).then(pokemons => {
         const resultados = pokemons.results;
-        
-        for(let p of resultados){
+
+        for (let p of resultados) {
             storePokemon(p.name)
         }
-    }).catch((error) => console.log("erro: "+error));
-    
-    console.log(pokemons);
-}
-
-function popular(){
+    }).catch((error) => console.log("erro: " + error));
     
 }
 
-consultPokemons(100);
+async function exibir(pokemon){
+    pokemonsLista.push(pokemon);
+    
+    let main = document.getElementById("main-card");
+
+    let card = document.createElement("div");
+    card.className = "card-pokemon";
+
+    let img = document.createElement("img");
+    img.src = pokemon.sprite;
+
+    let infos = document.createElement("div");
+    infos.className = "infos";
+    
+    let p = document.createElement("p");
+    p.innerHTML = "N° "+pokemon.id;
+    
+    let h1 = document.createElement("h1");
+    h1.innerHTML = pokemon.name;
+    
+    let types = document.createElement("div");
+    types.className = "types";
+
+    for(let t of pokemon.types){
+        let span = document.createElement("span");
+        span.className = "type-name"
+        span.innerHTML = t.type;
+        types.appendChild(span);
+    }
+
+    infos.appendChild(p);
+    infos.appendChild(h1);
+    infos.appendChild(types);
+
+    card.appendChild(img)
+    card.appendChild(infos);
+
+    main.appendChild(card);
+}
+
